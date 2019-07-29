49b750ff6f6c85fa4525dda1bab039925089f8c0
Robert Lai <rcylai@hotmail.com>
2019-07-28 21:50:58 -0700

    Manifest and initial script

---
+++ script.js
@@ -0,0 +1,23 @@
+var timeElapsed = 0;
+
+function clickMenuItem() {
+    var menuItem = document.querySelector('[role="menu"] [tabindex="0"]');
+    if (menuItem) {
+        menuItem.click();
+    } else {
+        setTimeout(clickMenuItem, 10);
+    }
+}
+
+function clickToggleButton() {
+    var toggleButton = document.querySelector('[aria-label="Top Tweets on"]');
+    if (toggleButton) {
+        toggleButton.click();
+        clickMenuItem();
+    } else if (timeElapsed < 1000) {
+        timeElapsed += 10;
+        setTimeout(clickToggleButton, 10);
+    }
+}
+
+clickToggleButton();
